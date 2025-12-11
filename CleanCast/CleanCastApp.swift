0e827ccd5746106e5f4b5e7ebb226b4ecf6d0ac6
Jonas Gunklach <jonasgunklach@me.com>
2025-12-11 10:23:13 +0100

    Initial Commit

---
+++ CleanCast/CleanCastApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanCastApp.swift
+//  CleanCast
+//
+//  Created by Jonas Gunklach on 11.12.25.
+//
+
+import SwiftUI
+
+@main
+struct CleanCastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
